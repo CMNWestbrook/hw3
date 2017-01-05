9ab1f46c2efb594f38bd472e03579b82013c7c94
Christina Mei Ngook Westbrook <christina@cwestbrook.com>
2017-01-05 07:18:21 +0000

    deleted stuff

--- accounting.py
+++ accounting.py
@@ -47,32 +47,3 @@ output_customer_payment_errors()
 #customer6_melons = 3
 #customer6_paid = 2.00
 
-#customer1_expected = customer1_melons * melon_cost
-#if customer1_expected != customer1_paid:
-#    print customer1_name, "paid {:.2f}, expected {:.2f}".format(
-#        customer1_paid, customer1_expected)
-#
-#customer2_expected = customer2_melons * melon_cost
-#if customer2_expected != customer2_paid:
-#   print customer2_name, "paid {:.2f}, expected {:.2f}".format(
-#       customer2_paid, customer2_expected)
-
-#customer3_expected = customer3_melons * melon_cost
-#if customer3_expected != customer3_paid:
-#    print customer3_name, "paid {:.2f}, expected {:.2f}".format(
-#        customer3_paid, customer3_expected)
-
-#customer4_expected = customer4_melons * melon_cost
-#if customer4_expected != customer4_paid:
-#    print customer4_name, "paid {:.2f}, expected {:.2f}".format(
-#        customer4_paid, customer4_expected)
-
-#customer5_expected = customer5_melons * melon_cost
-#if customer5_expected != customer5_paid:
-#    print customer5_name, "paid {:.2f}, expected {:.2f}".format(
-#       customer5_paid, customer5_expected)
-
-#customer6_expected = customer6_melons * melon_cost
-#if customer6_expected != customer6_paid:
-#    print customer6_name, "paid {:.2f}, expected {:.2f}".format(
-#        customer6_paid, customer6_expected)
